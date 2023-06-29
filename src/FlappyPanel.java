61bf7029c026ce46baffda76e8da1aa8ba6f75d7
Alexander Diaz <AlexDiaz6022000@gmail.com>
2023-06-29 13:29:51 -0700

    add graphics

---
+++ src/FlappyPanel.java
@@ -0,0 +1,34 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class FlappyPanel extends JPanel implements KeyListener, ActionListener {
+    public FlappyPanel(){
+        setSize(525,550);
+        setFocusable(true);
+        setBackground(Color.black);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+}
